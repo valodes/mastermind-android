8016bb2638b977409fae9643302b3ade334b0d74
Aiiisaka <theptitegeek@gmail.com>
2022-06-10 16:26:57 +0200

    Modification stats

--- app/src/main/java/com/maitredelesprit/database/AdaptateurStats.java
+++ app/src/main/java/com/maitredelesprit/database/AdaptateurStats.java
@@ -1,12 +1,15 @@
 package com.maitredelesprit.database;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.maitredelesprit.R;
@@ -53,6 +56,29 @@ public class AdaptateurStats extends RecyclerView.Adapter<AdaptateurStats.ViewHo
         holder.perdu.setText(String.valueOf(stats.getPerdu()));
         holder.classement.setText(String.valueOf(position+1)); // On commence à 1
         holder.ratio.setText(String.valueOf(stats.getRatio()));
+
+        switch (position) {
+            case 0 :
+                holder.pseudo.setTextColor(ContextCompat.getColor(context, R.color.or));
+                holder.classement.setTextColor(ContextCompat.getColor(context, R.color.or));
+                break;
+            case 1 :
+                holder.pseudo.setTextColor(ContextCompat.getColor(context, R.color.argent));
+                holder.classement.setTextColor(ContextCompat.getColor(context, R.color.argent));
+                break;
+            case 2 :
+                holder.pseudo.setTextColor(ContextCompat.getColor(context, R.color.bronze));
+                holder.classement.setTextColor(ContextCompat.getColor(context, R.color.bronze));
+                break;
+            default:
+                break;
+        }
+
+        if (position%2 == 0) {
+            holder.card_stats.setBackgroundColor(ContextCompat.getColor(context, R.color.md_theme_outline));
+        } else {
+            holder.card_stats.setBackgroundColor(ContextCompat.getColor(context, R.color.md_theme_surfaceVariant));
+        }
     }
 
     /**
@@ -79,6 +105,7 @@ public class AdaptateurStats extends RecyclerView.Adapter<AdaptateurStats.ViewHo
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView classement, pseudo, gagne, perdu, ratio;
+        private CardView card_stats;
 
         /**
          * Constructeur de la classe ViewHolder pour l'affichage des éléments dans la liste des stats
@@ -88,6 +115,7 @@ public class AdaptateurStats extends RecyclerView.Adapter<AdaptateurStats.ViewHo
          */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            card_stats = (CardView)itemView.findViewById(R.id.card_stats);
             classement = itemView.findViewById(R.id.classement);
             pseudo = itemView.findViewById(R.id.pseudo);
             gagne = itemView.findViewById(R.id.gagne);
